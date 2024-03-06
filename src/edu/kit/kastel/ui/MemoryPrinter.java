011cdbfd305bfa400883035cbdb1f0cfc6ff8df4
uiiux <uiiux@student.kit.edu>
2024-03-06 21:46:53 +0100

    Refactor MemoryPrinter to improve output formatting
    The MemoryPrinter's method to display memory cells has been updated for a better output format. Adjustments include calculating the maximum length of each field (name, first argument, and second argument) to align the output neatly. The changes enhance the readability of data presented in the console.

--- src/edu/kit/kastel/ui/MemoryPrinter.java
+++ src/edu/kit/kastel/ui/MemoryPrinter.java
@@ -61,7 +61,7 @@ public class MemoryPrinter {
             int secondArgument = currentCell.getSecondArgument();
             int currentCellPosition = memory.getPosition(currentCell);
             String formattedLine = String.format(
-                    "%s %" + maxPosLength + "d: %-" + maxNameLength + "s | %" + maxFirstArgLength + "d | %"
+                    "%s %" + maxPosLength + "d: %5s | %" + maxFirstArgLength + "d | %"
                             + maxSecondArgLength + "d" + System.lineSeparator(),
                     currentCell.getCurrentSymbol(), currentCellPosition, name, firstArgument, secondArgument);
             sb.append(formattedLine);
